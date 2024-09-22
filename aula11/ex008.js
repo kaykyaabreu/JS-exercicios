6a3e933cfae630e5eeb8f710930d019bb5e6084a
kaykyaabreu <kaykyhulk100@gmail.com>
2024-09-22 18:57:58 -0300

    Aula 11 – Condições (Parte 1)

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.8
+console.log (`a velocidade do seu carro e ${vel}`)
+if(vel > 60){
+    console.log ('voce ultrapassou a velocidade permitida')
+}
+console.log ('dirija sempre usando cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'Brasil'
+if (pais =! "Brasil"){
+    console.log('voce e extrangeiro')
+}else{
+    console.log('voce e brasileiro')
+}
