e4483f6051f039da816e58566dbe2d480b99397c
kaykyaabreu <kaykyhulk100@gmail.com>
2024-09-23 15:28:26 -0300

    Aula 12 – Condições (Parte 2)

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 80
+console.log(`voce tem ${idade} anos`)
+if(idade >= 16 && idade < 18 || idade > 65 ){
+    console.log('voto opcional')
+}else if(idade < 18 ){ 
+    console.log('nao vota')
+}else{
+    console.log('vota')
+}
